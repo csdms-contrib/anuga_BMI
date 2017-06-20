e6136841271e6fe32a46b3511c1658e524cca27e
Mark Piper <mark.piper@colorado.edu>
2017-06-20 15:43:50 -0600

    Make BmiAnuga class public in package
    This makes it easier to import. Keep AnugaSolver private.

--- anuga_bmi/anugaBMI.py
+++ anuga_bmi/anugaBMI.py
@@ -7,7 +7,7 @@ import numpy as np
 import yaml
 from basic_modeling_interface import Bmi
 
-from anuga_solver import AnugaSolver
+from anuga_bmi.anuga_solver import AnugaSolver
 
 
 class BmiAnuga(Bmi):
